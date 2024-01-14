084a607f2bc60114c368fb018c14107eae78bfc7
ZX夏夜之风 <snwcreations@qq.com>
2024-01-14 11:28:03 +0800

    feat: use JOptSimple's help generator

--- src/main/kotlin/io/jadon/mapping_generator/main.kt
+++ src/main/kotlin/io/jadon/mapping_generator/main.kt
@@ -9,20 +9,20 @@ val GLOBAL_FOLDER = File("mappings")
 fun main(args: Array<String>) {
     val time = System.currentTimeMillis()
     GLOBAL_FOLDER.mkdirs()
+	val parser = OptionParser()
+	val mcOpt = parser.accepts("mc").withRequiredArg().ofType(String::class.java)
+	val yarnOpt = parser.accepts("yarn").withRequiredArg().ofType(String::class.java)
+//		val yInOpt = parser.accepts("yarn").withRequiredArg().ofType(File::class.java)
+	val spigotClsOpt = parser.accepts("spigotCls").withRequiredArg().ofType(File::class.java)
+	val spigotMemberIn = parser.accepts("spigotMember").withOptionalArg().ofType(File::class.java)
 
 	if (args.isEmpty()) {
-		println("Usage: java -jar mappings-generator.jar --mc <minecraftversion> --yarn <file> --spigotCls <file> [--spigotMember file]")
+		parser.printHelpOn(System.out)
 		println("Possible values for <minecraftversion> :")
 		MinecraftVersion.values().forEach {
 			println("\t" + it.mcVersion);
 		}
 	} else {
-		val parser = OptionParser()
-		val mcOpt = parser.accepts("mc").withRequiredArg().ofType(String::class.java)
-		val yarnOpt = parser.accepts("yarn").withRequiredArg().ofType(String::class.java)
-//		val yInOpt = parser.accepts("yarn").withRequiredArg().ofType(File::class.java)
-		val spigotClsOpt = parser.accepts("spigotCls").withRequiredArg().ofType(File::class.java)
-		val spigotMemberIn = parser.accepts("spigotMember").withOptionalArg().ofType(File::class.java)
 		val optSet = parser.parse(*args)
 
 	    MinecraftVersion.findByMCVersion(optSet.valueOf(mcOpt))!!.write(optSet.valueOf(yarnOpt), optSet.valueOf(spigotClsOpt), optSet.valueOf(spigotMemberIn), GLOBAL_FOLDER)
